2180b3ea95b083697d384b3c6f7c03db7004260d
Kuzmin Anton <kuz942@gmail.com>
2025-04-07 10:12:35 +0200

    Revert "Send logs to OpenTelemetry collector"

--- downstream/src/main/kotlin/com/kuzmin/open/telemetry/downstream/DownstreamConfiguration.kt
+++ downstream/src/main/kotlin/com/kuzmin/open/telemetry/downstream/DownstreamConfiguration.kt
@@ -1,21 +1,6 @@
 package com.kuzmin.open.telemetry.downstream
 
-import io.opentelemetry.api.GlobalOpenTelemetry
-import io.opentelemetry.api.OpenTelemetry
-import io.opentelemetry.api.trace.Tracer
-import io.opentelemetry.context.propagation.ContextPropagators
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DownstreamConfiguration {
-
-    @Bean
-    fun openTelemetry(): OpenTelemetry = GlobalOpenTelemetry.get()
-
-    @Bean
-    fun tracer(openTelemetry: OpenTelemetry): Tracer = openTelemetry.getTracer("")
-
-    @Bean
-    fun contextPropagators(openTelemetry: OpenTelemetry): ContextPropagators = openTelemetry.propagators
-}
+class DownstreamConfiguration
